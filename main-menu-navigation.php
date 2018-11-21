7850ee4f30312beed010dbbfd7b6a3d4e05ecce2
Jp <johnpaulbalanon6@gmail.com>
2018-11-21 18:48:10 +0800

    ..

---
+++ main-menu-navigation.php
@@ -0,0 +1,14 @@
+ <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
+  <li class="nav-item">
+	<a href="index.html" class="nav-link <?=($page=="index")?' active':''?>"><i class="fe fe-home"></i> Home</a>
+  </li>
+  <li class="nav-item">
+	<a href="javascript:void(0)" class="nav-link <?=($page=="groups")?' active':''?>" data-toggle="dropdown"><i class="fe fe-settings"></i> Maitenance</a>
+	<div class="dropdown-menu dropdown-menu-arrow">
+	  <a href="groups.html" class="dropdown-item <?=($page=="groups")?' active':''?>">Groups</a>
+	  <a href="users.html" class="dropdown-item<?=($page=="users")?' active':''?>">Users</a>
+	  <a href="discounts.html" class="dropdown-item">Discounts</a>
+	  <a href="suppliers.html" class="dropdown-item">Suppliers</a>
+	</div>
+  </li>
+</ul>
