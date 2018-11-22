cd8ef03781b7b1d789ec60c36dbe490b789576d0
Jp <johnpaulbalanon6@gmail.com>
2018-11-22 22:28:06 +0800

    sql

--- yessamin.sql
+++ yessamin.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 11, 2018 at 10:24 AM
+-- Generation Time: Nov 22, 2018 at 02:27 PM
 -- Server version: 5.7.11
 -- PHP Version: 7.0.3
 
@@ -23,21 +23,119 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `discounts`
+--
+
+CREATE TABLE `discounts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `groups`
 --
 
 CREATE TABLE `groups` (
   `id` int(11) NOT NULL,
   `group_name` varchar(255) DEFAULT NULL,
-  `group_description` varchar(255) DEFAULT NULL
+  `group_description` varchar(255) DEFAULT NULL,
+  `privileges` longtext
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `groups`
 --
 
-INSERT INTO `groups` (`id`, `group_name`, `group_description`) VALUES
-(1, 'Admin', 'Administrator');
+INSERT INTO `groups` (`id`, `group_name`, `group_description`, `privileges`) VALUES
+(1, 'Admin', 'Adminitrator', '225B7B5C2269645C223A5C2264617368626F6172645C222C5C226465736372697074696F6E5C223A5C2244617368626F6172645C222C5C2270726976696C656765735C223A5B7B5C2269645C223A312C5C226465736372697074696F6E5C223A5C2253686F772044617368626F6172645C222C5C2276616C75655C223A747275657D5D7D2C7B5C2269645C223A5C226D61696E74656E616E63655C222C5C226465736372697074696F6E5C223A5C224D61696E74656E616E63655C222C5C2270726976696C656765735C223A5B7B5C2269645C223A312C5C226465736372697074696F6E5C223A5C2253686F77204D61696E74656E616E63655C222C5C2276616C75655C223A747275657D5D7D5D22'),
+(2, 'User', 'User', '225B7B5C2269645C223A5C2264617368626F6172645C222C5C226465736372697074696F6E5C223A5C2244617368626F6172645C222C5C2270726976696C656765735C223A5B7B5C2269645C223A312C5C226465736372697074696F6E5C223A5C2253686F772044617368626F6172645C222C5C2276616C75655C223A747275657D5D7D2C7B5C2269645C223A5C226D61696E74656E616E63655C222C5C226465736372697074696F6E5C223A5C224D61696E74656E616E63655C222C5C2270726976696C656765735C223A5B7B5C2269645C223A312C5C226465736372697074696F6E5C223A5C2253686F77204D61696E74656E616E63655C222C5C2276616C75655C223A66616C73657D5D7D5D22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`id`, `name`, `description`) VALUES
+(1, 'Natasha', 'Natasha'),
+(2, 'asd', 'asd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers_categories`
+--
+
+CREATE TABLE `suppliers_categories` (
+  `id` int(11) NOT NULL,
+  `suppliers_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `suppliers_categories`
+--
+
+INSERT INTO `suppliers_categories` (`id`, `suppliers_id`, `name`, `description`) VALUES
+(1, 2, 'fg', 'fg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers_discounts`
+--
+
+CREATE TABLE `suppliers_discounts` (
+  `id` int(11) NOT NULL,
+  `suppliers_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `suppliers_discounts`
+--
+
+INSERT INTO `suppliers_discounts` (`id`, `suppliers_id`, `name`, `description`) VALUES
+(1, NULL, 'Basic Discount', 'Basic'),
+(2, NULL, 'Top Seller Discount', 'Top Seller'),
+(3, NULL, 'Outright Discount', 'Outright'),
+(4, NULL, 'Special Discount', 'Special'),
+(5, NULL, 'SL Discount', 'SL');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers_main_categories`
+--
+
+CREATE TABLE `suppliers_main_categories` (
+  `id` int(11) NOT NULL,
+  `suppliers_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `suppliers_main_categories`
+--
+
+INSERT INTO `suppliers_main_categories` (`id`, `suppliers_id`, `name`, `description`) VALUES
+(1, 2, 'asd', 'asdd');
 
 -- --------------------------------------------------------
 
@@ -60,17 +158,51 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `middlename`, `lastname`, `username`, `password`, `groups`) VALUES
-(1, 'John Paul', 'Garcia', 'Balanon', 'admin', 'admin', NULL);
+(1, 'John Paul', 'G', 'Balanon', 'admin', 'admin', 1),
+(2, 'Sly', 'B', 'Flores', 'sly', 'legend', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `discounts`
+--
+ALTER TABLE `discounts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `groups`
 --
 ALTER TABLE `groups`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `suppliers_categories`
+--
+ALTER TABLE `suppliers_categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `suppliers_id` (`suppliers_id`);
+
+--
+-- Indexes for table `suppliers_discounts`
+--
+ALTER TABLE `suppliers_discounts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `suppliers_id` (`suppliers_id`);
+
+--
+-- Indexes for table `suppliers_main_categories`
+--
+ALTER TABLE `suppliers_main_categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `suppliers_id` (`suppliers_id`);
 
 --
 -- Indexes for table `users`
@@ -83,15 +215,40 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `discounts`
+--
+ALTER TABLE `discounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `suppliers_categories`
+--
+ALTER TABLE `suppliers_categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `suppliers_discounts`
+--
+ALTER TABLE `suppliers_discounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `suppliers_main_categories`
+--
+ALTER TABLE `suppliers_main_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
